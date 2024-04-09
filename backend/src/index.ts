3c4b346919d75da5770b141a5272cf1d2b44be11
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-04-09 10:39:26 +0530

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express ,{Request , Response}from 'express'
+import cors from 'cors'
+import  'dotenv/config'
+
+const app = express()
+app.use(cors())
+
+app.use(express.json())
+
+app.get('/test',async (req:Request , res:Response) => {
+    res.json({message:"Hello"})
+})
+
+app.listen( 7000 , () => {
+    console.log('Server is running on port 7000')
+})
