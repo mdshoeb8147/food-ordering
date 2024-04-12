45346d14ea80dac2bd8e66dae7ada864353d9ef3
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-04-12 22:11:53 +0530

    nav responsive

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -3,21 +3,40 @@ import { Button } from "./ui/button"
 import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "./ui/sheet"
 import { Separator } from "./ui/separator"
 const MobileNav = () => {
+//   return (
+// <Sheet>
+// <SheetTrigger>
+//     <Menu className="text-orange-500"/>
+// </SheetTrigger>
+// <SheetContent>
+//     <SheetTitle>
+//         <span>Welcome to Eats.com</span>
+//     </SheetTitle>
+//     <Separator/>
+//     <SheetDescription className="flex">
+// <Button className="flex-1 font-bold bg-orange-500 ">Log In</Button>
+//     </SheetDescription>
+// </SheetContent>
+// </Sheet>
+// )
+
   return (
-<Sheet>
-<SheetTrigger>
-    <Menu className="text-orange-500"/>
-</SheetTrigger>
-<SheetContent>
-    <SheetTitle>
-        <span>Welcome to Eats.com</span>
-    </SheetTitle>
-    <Separator/>
-    <SheetDescription className="flex">
-<Button className="flex-1 font-bold bg-orange-500 ">Log In</Button>
-    </SheetDescription>
-</SheetContent>
-</Sheet>
-)
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="text-orange-500" />
+      </SheetTrigger>
+      <SheetContent className="space-y-3">
+        <SheetTitle>
+         <span>Welcome to Eats.com</span>
+        </SheetTitle>
+        <Separator />
+        <SheetDescription className="flex flex-col gap-4">
+        <Button className="flex-1 font-bold bg-orange-500 ">Log In</Button>
+
+        </SheetDescription>
+      </SheetContent>
+    </Sheet>
+  );
+
 }
 export default MobileNav
