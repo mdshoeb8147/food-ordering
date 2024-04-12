2955e1be6aca4b36b9b9a9145969d8c6f52fdcc9
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-04-12 21:50:56 +0530

    layout

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -4,7 +4,7 @@ import Layout from "./layouts/layout"
 const AppRoutes = () => {
   return (
 <Routes>
-<Route path="/" element={<Layout/>} /> 
+<Route path="/" element={<Layout> Home Page</Layout>} /> 
 <Route path="/user-profile" element={<span>User-Page </span>} />
 <Route path="*" element={<Navigate to='/'/>} /> 
 
