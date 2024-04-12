461aeaeda4ace3e9f069382a92976d4f2f0809e5
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-04-12 22:01:44 +0530

    mobnavbar

---
+++ frontend/src/components/MobileNav.tsx
@@ -0,0 +1,23 @@
+import { Menu } from "lucide-react"
+import { Button } from "./ui/button"
+import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "./ui/sheet"
+import { Separator } from "./ui/separator"
+const MobileNav = () => {
+  return (
+<Sheet>
+<SheetTrigger>
+    <Menu className="text-orange-500"/>
+</SheetTrigger>
+<SheetContent>
+    <SheetTitle>
+        <span>Welcome to Eats.com</span>
+    </SheetTitle>
+    <Separator/>
+    <SheetDescription className="flex">
+<Button className="flex-1 font-bold bg-orange-500 ">Log In</Button>
+    </SheetDescription>
+</SheetContent>
+</Sheet>
+)
+}
+export default MobileNav
